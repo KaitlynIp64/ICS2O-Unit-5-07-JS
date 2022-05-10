a65b180b769ccc05a2e63d60e0eb9700987c1602
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-05-10 17:22:00 +0000

    fixed code

--- js/script.js
+++ js/script.js
@@ -3,50 +3,25 @@
 // Created by: Kaitlyn Ip
 // Created on: Apr 2022
 // This file contains xxx
-
 "use strict"
-
 /**
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-Unit-5-06-JS/sw.js", {
-    scope: "/ICS2O-Unit-5-06-JS/",
-  })
+  navigator.serviceWorker.register("/ICS20-Unit-5-07-JS/sw.js", {
+    scope: "/ICS20-Unit-5-07-JS/",
+  });
 }
 
 /**
  * Input
  */
-function myButtonClicked () {
-  // This code will multiply your two numbers with while loops (repeated addition)
-
-  const counter1 = parseInt(document.getElementById('counter1').value)
-	const counter2 = parseInt(document.getElementById('counter2').value)
-  var addedNumber = 0
-  var answer = 0
-
-  if (counter1 > 0 && counter2 > 0) {
-    while (addedNumber < counter2) {
-      addedNumber = addedNumber + 1;
-      answer = answer + counter1;
-    }
-  } else if (counter1 < 0 && counter2 < 0) {
-    while (addedNumber > counter2) {
-      addedNumber = addedNumber - 1;
-      answer = answer - counter1;
-    }
-  } else if (counter1 > 0 && counter2 < 0) {
-    while (addedNumber > counter2) {
-      addedNumber = addedNumber - 1;
-      answer = answer - counter1;
-    }
-  } else if (counter1 < 0 && counter2 > 0) {
-    while (addedNumber < counter2) {
-      addedNumber = addedNumber + 1;
-      answer = answer + counter1;
-    }
-  }
+function myButtonClicked() {
+  const number = parseInt(document.getElementById("number").value);
+  let sum = 0;
   
-  document.getElementById('answer').innerHTML = counter1 + " x " + addedNumber + " = " + answer;
+  for (let counter = 1; counter <= number; counter++) {
+    sum += counter;
+  }
+  document.getElementById("answer").innerHTML = "The answer is: " + sum;
 }
